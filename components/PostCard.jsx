5f2d4c8904a06bb42a65ca9ee6435f51baf64b0d
coimbrox <gabao.coimbra@gmail.com>
2022-03-03 11:05:22 -0300

    Adicionando Post2

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -10,7 +10,7 @@ const PostCard = ({ post }) => {
       <div className="relative iverflow-hidden shadow-md pb-80 mb-6">
         <img src={post.imagem.url} alt={post.titulo} className='object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg' />
       </div>
-      <h1 className='transition duration-700 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold'>
+      <h1 className='transition duration-700 text-center mb-8 cursor-pointer hover:text-orange-600 text-3xl font-semibold'>
         <Link href={`/post/${post.link}`} >
           {post.titulo}
         </Link>
@@ -36,7 +36,7 @@ const PostCard = ({ post }) => {
       <p className='text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8'>{post.excerpt}</p>
       <div className="text-center">
         <Link href={`/post/${post.link}`}>
-          <span className='transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer'>
+          <span className='transition duration-500 transform hover:-translate-y-1 inline-block bg-orange-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer'>
             Continue Lendo
           </span>
         </Link>
