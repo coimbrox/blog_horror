68e55c1e96289aeffb4316a74c85dc6b4f7a10f1
coimbrox <gabao.coimbra@gmail.com>
2022-03-03 10:52:54 -0300

    add excerpt

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -33,6 +33,7 @@ const PostCard = ({ post }) => {
           </span>
         </div>
       </div>
+      <p className='text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8'>{post.excerpt}</p>
     </div>
   )
 }
